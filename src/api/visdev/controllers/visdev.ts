6ea3633183d755f2c9a7fc9f3361d034aa3db980
Sean Norwood <norwood.sean@gmail.com>
2023-06-09 14:47:36 -0500

    Initial commit
    Signed-off-by: Sean Norwood <norwood.sean@gmail.com>

---
+++ src/api/visdev/controllers/visdev.ts
@@ -0,0 +1,7 @@
+/**
+ * visdev controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::visdev.visdev');
---
+++ src/api/visdev/routes/visdev.ts
@@ -0,0 +1,7 @@
+/**
+ * visdev router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::visdev.visdev');
